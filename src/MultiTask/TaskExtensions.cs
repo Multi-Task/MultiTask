363d3494f65bffa8e95bfea973d23a3673b5968c
klings <klingsen@gmail.com>
2019-12-13 22:45:47 +0100

    Add extension method to throw AggregateException

---
+++ src/MultiTask/TaskExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace MultiTask
+{
+    public static class TaskExtensions
+    {
+        public static async Task AggregateExceptions(this Task task)
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch
+            {
+                if (task.IsFaulted)
+                {
+                    ExceptionDispatchInfo.Capture(task.Exception).Throw();
+                }
+                throw;
+            }
+        }
+
+        public static async Task<T> AggregateExceptions<T>(this Task<T> task)
+        {
+            try
+            {
+                return await task.ConfigureAwait(false);
+            }
+            catch
+            {
+                if (task.IsFaulted)
+                {
+                    ExceptionDispatchInfo.Capture(task.Exception).Throw();
+                }
+                throw;
+            }
+        }
+    }
+}
